e1cfe1799b275a75a3ace1311f182e40efbbbddc
Brandon <74200568+bsalinassanchez@users.noreply.github.com>
2022-01-27 20:00:24 -0800

    Update MarkdownParse.java
    Updated comments

--- MarkdownParse.java
+++ MarkdownParse.java
@@ -6,26 +6,19 @@ import java.nio.file.Paths;
 import java.util.ArrayList;
 
 public class MarkdownParse {
+    //Updated method parameters from: String markdown -> String[] markdown
     public static ArrayList<String> getLinks(String[] markdown) {
         ArrayList<String> toReturn = new ArrayList<>();
-        // find the next [, then find the ], then find the (, then take up to
-        // the next )
-        //int currentIndex = 0;
-        //while(currentIndex < markdown.length()) {
-        //    int nextOpenBracket = markdown.indexOf("[", currentIndex);
-        //    int nextCloseBracket = markdown.indexOf("]", nextOpenBracket);
-        //    int openParen = markdown.indexOf("(", nextCloseBracket);
-        //    int closeParen = markdown.indexOf(")", openParen);
-        //    toReturn.add(markdown.substring(openParen + 1, closeParen));
-        //    currentIndex = closeParen + 1;
-        //    System.out.println("current index: " + currentIndex);
-        //}
-
         //using a for loop instead of a while loop(different implementation of method)
+        //loops through each line in a markdown file
         for(int i = 0; i < markdown.length; i++) {
+            //checks that a line is not empty
             if(markdown[i].length() != 0) {
-                if(markdown[i].contains("](")) {
-                    int indexOfMarker = markdown[i].indexOf("](");
+                //checks that a line contains brackets and parenthesis for links [...](...)
+                //in the correct order
+                if(markdown[i].contains("[") && markdown[i].contains("](") && markdown[i].contains(")")) {
+                    int indexOfOpenBracket = markdown[i].indexOf("[");
+                    int indexOfMarker = markdown[i].indexOf("](", indexOfOpenBracket);
                     int closeParen = markdown[i].indexOf(")", indexOfMarker);
                     toReturn.add(markdown[i].substring(indexOfMarker+2, closeParen));
                 }
@@ -37,13 +30,8 @@ public class MarkdownParse {
         if(args.length == 0) {
             System.out.println("no argument");
             return;
-        }
-		//Path fileName = Path.of(args[0]);
-	    //String contents = Files.readString(fileName);
-        //split contents into string array(This idea is inspired by a CSE11 PA7)
+        }	
         String[] splitContents = Files.readAllLines(Paths.get(args[0])).toArray(String[]::new);
-
-        //System.out.println(contents);
 
         ArrayList<String> links = getLinks(splitContents);
         System.out.println(links);
