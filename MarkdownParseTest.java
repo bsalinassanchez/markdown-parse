281c6e91ae99f915c57ae2f6da669787f954d415
Brandon <74200568+bsalinassanchez@users.noreply.github.com>
2022-02-02 16:50:47 -0800

    Update MarkdownParseTest.java
    expected output different from actual

--- MarkdownParseTest.java
+++ MarkdownParseTest.java
@@ -3,11 +3,18 @@ import org.junit.*;
 import java.util.List;
 import java.io.IOException;
 import java.nio.file.Files;
+import java.nio.file.Path;
 import java.nio.file.Paths;
 
 public class MarkdownParseTest {
     @Test
     public void addition() {
         assertEquals(2, 1 + 1);
+    }
+    @Test
+    public void getLinks() throws IOException {
+        assertEquals("checks that the method getLinks give correct output",
+            List.of("https://something.com", "some-page.html").toString(), 
+            MarkdownParse.getLinks(Files.readString(Paths.get("/Users/brandon/Documents/GitHub/markdown-parse/test-file.md")).toString()));
     }
 }
